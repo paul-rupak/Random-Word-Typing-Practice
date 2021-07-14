e65750f5eb14d95b685ecd7a93834355c591b56b
himan saha <himansaha7@gmail.com>
2021-07-14 21:31:53 +0530

    setting_dialog box added

---
+++ Src/setting_Dialog/setting.Designer.cs
@@ -0,0 +1,88 @@
+﻿
+namespace Typing_Practice
+{
+    partial class setting
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.del_fav_btn = new System.Windows.Forms.Button();
+            this.set_btn = new System.Windows.Forms.Button();
+            this.add_fav_btn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // del_fav_btn
+            // 
+            this.del_fav_btn.Location = new System.Drawing.Point(387, 43);
+            this.del_fav_btn.Name = "del_fav_btn";
+            this.del_fav_btn.Size = new System.Drawing.Size(146, 35);
+            this.del_fav_btn.TabIndex = 1;
+            this.del_fav_btn.Text = "Delete selected favourite";
+            this.del_fav_btn.UseVisualStyleBackColor = true;
+            this.del_fav_btn.Click += new System.EventHandler(this.del_fav_btn_Click);
+            // 
+            // set_btn
+            // 
+            this.set_btn.Location = new System.Drawing.Point(182, 474);
+            this.set_btn.Name = "set_btn";
+            this.set_btn.Size = new System.Drawing.Size(75, 23);
+            this.set_btn.TabIndex = 2;
+            this.set_btn.Text = "Set";
+            this.set_btn.UseVisualStyleBackColor = true;
+            this.set_btn.Click += new System.EventHandler(this.set_btn_Click);
+            // 
+            // add_fav_btn
+            // 
+            this.add_fav_btn.Location = new System.Drawing.Point(374, 474);
+            this.add_fav_btn.Name = "add_fav_btn";
+            this.add_fav_btn.Size = new System.Drawing.Size(146, 23);
+            this.add_fav_btn.TabIndex = 3;
+            this.add_fav_btn.Text = "Add to favourite";
+            this.add_fav_btn.UseVisualStyleBackColor = true;
+            this.add_fav_btn.Click += new System.EventHandler(this.add_fav_btn_Click);
+            // 
+            // setting
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(918, 532);
+            this.Controls.Add(this.add_fav_btn);
+            this.Controls.Add(this.set_btn);
+            this.Controls.Add(this.del_fav_btn);
+            this.Name = "setting";
+            this.Text = "setting";
+            this.Load += new System.EventHandler(this.setting_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        
+        private System.Windows.Forms.Button del_fav_btn;
+        private System.Windows.Forms.Button set_btn;
+        private System.Windows.Forms.Button add_fav_btn;
+    }
+}
---
+++ Src/setting_Dialog/setting.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.IO;
+
+
+
+namespace Typing_Practice
+{
+    public partial class setting : Form
+    {
+
+        private const string pathSettingJSON = "D:\\favourite.json";
+        public setting()
+        {
+            InitializeComponent();
+        }
+
+        public class favourite
+        {
+            public string name { get; set; }
+
+            public List<string> selected_chars { get; set; }
+        }
+
+        public List<Button> buttons = new List<Button>();
+
+        public List<favourite> favourites = new List<favourite>();
+
+        public ComboBox favourite_box = new ComboBox();
+
+        private void setting_Load(object sender, EventArgs e)
+        {
+            
+            for (char c = '!'; c <= '}'; c++)
+            {
+                Button testbutton = new Button();
+                testbutton.Text = c.ToString();
+                testbutton.Location = new Point(70 + 50 * (buttons.Count % 15), (buttons.Count / 15) * 50 + 110);
+                testbutton.Size = new Size(45, 45);
+                testbutton.BackColor = Color.White;
+                testbutton.Visible = true;
+                testbutton.BringToFront();
+                this.Controls.Add(testbutton);
+                testbutton.Click += new EventHandler(key_btn_Click);
+
+                buttons.Add(testbutton);
+            }
+
+            
+
+            favourite_box.Location = new Point(150, 50);
+            favourite_box.Size = new Size(100, 30);
+            favourite_box.Text = "select favourite";
+            favourite_box.BringToFront();
+            this.Controls.Add(favourite_box);
+            favourite_box.Visible = true;
+            favourite_box.SelectedIndexChanged += new EventHandler(fav_box_SelectedIndexChanged);
+
+
+            var settingJSON = File.ReadAllText(@pathSettingJSON);
+            favourites = JsonConvert.DeserializeObject<List<favourite>>(settingJSON);
+
+            foreach (var favourite in favourites)
+            {
+                    favourite_box.Items.Add(favourite.name);
+            }
+
+        }
+
+        private void key_btn_Click(object sender, EventArgs e)
+        {
+            Button clicked_button = (Button)sender;
+            if (clicked_button.BackColor == Color.White)
+            {
+                clicked_button.BackColor = Color.Green;
+                clicked_button.ForeColor = Color.White;
+            }
+
+            else
+            {
+                clicked_button.BackColor = Color.White;
+                clicked_button.ForeColor = Color.Black;
+
+            }
+        }
+
+        
+        private void set_btn_Click(object sender, EventArgs e)
+        {
+
+            var settingJSON = File.ReadAllText(@pathSettingJSON);
+            favourites = JsonConvert.DeserializeObject<List<favourite>>(settingJSON);
+
+            favourite temp = favourites.Find(i => i.name == "current_setting");
+            temp.selected_chars.Clear();
+
+            foreach (var button in buttons)
+            {
+                if (button.BackColor == Color.Green)
+                {
+                    temp.selected_chars.Add(button.Text);
+                }
+            }
+
+            string json = JsonConvert.SerializeObject(favourites.ToArray());
+
+            //write string to file
+            File.WriteAllText(@pathSettingJSON, json);
+
+
+        }
+
+        private void add_fav_btn_Click(object sender, EventArgs e)
+        {
+            var myform = new Enter_fav_name(this);
+            myform.Show();
+        }
+
+        private void fav_box_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            foreach (var favourite in favourites)
+            {
+                if (favourite.name.Equals(((ComboBox)sender).SelectedItem))
+                {
+                    foreach (var btn in buttons)
+                    {
+                        var temp = favourite.selected_chars.Find(i => i == btn.Text);
+
+                        if (temp != null)
+                        {
+                            btn.BackColor = Color.Green;
+                            btn.ForeColor = Color.White;
+                        }
+
+                        else
+                        {
+                            btn.BackColor = Color.White;
+                            btn.ForeColor = Color.Black;
+                        }
+                    }
+
+                    break;
+                }
+            }
+        }
+
+        private void del_fav_btn_Click(object sender, EventArgs e)
+        {
+            if (favourite_box.SelectedIndex < 0)
+            {
+                MessageBox.Show("no item selected");
+                return;
+            }
+
+            if (favourite_box.SelectedItem.Equals("current_setting"))
+            {
+                MessageBox.Show("current setting can't be deleted");
+                return;
+            }
+
+            else
+            {
+                favourites.Remove(favourites.Find(i => i.name.Equals(favourite_box.SelectedItem)));
+
+                favourite_box.Items.Remove(favourite_box.SelectedItem);
+
+
+                string json = JsonConvert.SerializeObject(favourites.ToArray());
+
+                //write string to file
+                File.WriteAllText(@pathSettingJSON, json);
+            }
+        }
+    }
+}
